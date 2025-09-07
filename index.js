f1a1c40c5477f6deb0f1911d16dc1e9515f9768d
Dev Singh <devrajpu18vk@gmail.com>
2025-09-07 23:13:30 +0530

    f

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-require ('dotenv').config()
-const express = require('express')
- const app = express()
-const port = 3000
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true ,  
+    methods: ['GET', 'POST', 'DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -11,6 +26,8 @@ app.get('/twitter',(req,res)=>{
     res.send('winterxback')
 })
 
+const port = process.env.PORT || 3000;
+
 app.get('/login', (req,res)=>{
     res.send('<h1>pls login kro <h1/>')
 })
@@ -18,6 +35,12 @@ app.get('/login', (req,res)=>{
 app.get('/youtube', (req,res)=> {
     res.send('<h2>fffg <h2/>')
 })
-app.listen(process.env.PORT, () => {
+
+
+
+
+
+
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
