c86a63a33ef3870f25c29a0304287d2dcb074755
Dev Singh <devrajpu18vk@gmail.com>
2025-09-09 00:17:21 +0530

    e

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,15 @@
 import mongoose from  "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+  
 
 
 mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("connected to mongodb");
+     
+})
+.catch((err)=>{
+    console.log("error");
+    
+});
